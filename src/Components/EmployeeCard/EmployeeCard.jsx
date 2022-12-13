d974ffd0d5bdeba077087eaa44ecd50536557733
Claire <claire.huizenga@outlook.com>
2022-12-13 13:48:12 +1100

    fixed fragment errors

--- src/Components/EmployeeCard/EmployeeCard.jsx
+++ src/Components/EmployeeCard/EmployeeCard.jsx
@@ -6,15 +6,13 @@ const EmployeeCard = (props) => {
   const { name, role } = props.employee;
 
   return (
-    <>
-      <div className={Styles.card}>
-        <h2 data-testid="name">Name: {name}</h2>
-        <h3 data-testid="role">Role: {role}</h3>
-        <section>
-          <Counter />
-        </section>
-      </div>
-    </>
+    <div className={Styles.card}>
+      <h2 data-testid="name">Name: {name}</h2>
+      <h3 data-testid="role">Role: {role}</h3>
+      <section>
+        <Counter />
+      </section>
+    </div>
   );
 };
 
